cb92af51692de8ecbd136242649d40984934d310
Dante Augusto <43281304+DanteAugusto@users.noreply.github.com>
2023-05-05 17:29:22 -0300

    Update instruction_memory.h

--- instruction_memory.h
+++ instruction_memory.h
@@ -2,19 +2,37 @@
 #include <iostream>
 
 SC_MODULE(instruction_memory) {
-    sc_in<sc_uint<32>> write_inst;
-    int index;
+    // sc_in_clk clk;
+    sc_in<sc_uint<25>> write_inst;
+    // sc_in<sc_uint<5>> current;
+    // sc_in<sc_uint<5>> last;
     sc_uint<32> inst[32];
+    // sc_in<bool> sigWriteIM;
+    // sc_in<sc_uint<5>> pointPC;
+    // sc_signal<bool> bubble;
+    // sc_out<sc_uint<32>> dataOut;
 
-    void inst_put(){
-        inst[index] = write_inst.read();
-        index++;
-    }
+    // void next_inst(){
+    //     if(!bubble.read() && !sigWriteIM.read()){
+    //         dataOut.write(inst[current.read()]);
+    //     }else{
+    //         dataOut.write(0); //TODO: se der ruim, verificar aqui
+    //     }
+    // }
+
+    // void inst_put(){
+    //     if(sigWriteIM.read()){
+    //         inst[last.read()] = write_inst.read();
+    //     }
+    // }
 
     SC_CTOR(instruction_memory) {
         for (int i = 0; i < 32; i++) {
             inst[i] = 0;
         }
-        index = 0;
+        // SC_METHOD(inst_put);
+        // sensitive << write_inst;
+        // SC_METHOD(next_inst);
+        // sensitive << clk.pos();
     }
 };
